dc166f1117a43594053288f2aa9b7d18b043cedc
Miguel <miguelfranciscodasilvasalessal@gmail.com>
2025-09-16 09:29:26 -0300

    commit servo motor

--- temperatura.ino
+++ temperatura.ino
@@ -5,12 +5,17 @@
 #define Botao 6
 DHT11 dht11(2);
 
+Servo ventilacao; // Declaramos o servo fora do loop
+
 void setup() {
   pinMode(6, INPUT);
   pinMode(4, OUTPUT);
 
   Serial.begin(9600);
   dht11.setDelay(500);
+
+  ventilacao.attach(ServoMotor); // Inicializa o servo no pino definido
+  ventilacao.write(0);           // Começa fechado
 }
 
 void loop() {
@@ -23,4 +28,18 @@ void loop() {
     } else {
         Serial.println(DHT11::getErrorString(temperatura));
     }
-}dsadsadsad
+
+    if (temperatura <= 20) {          
+      ventilacao.write(0);      // 0% abertura
+    } else if (temperatura <= 25) {   
+      ventilacao.write(45);     // 25% abertura
+    } else if (temperatura <= 30) {   
+      ventilacao.write(90);     // 50% abertura
+    } else if (temperatura <= 35) {   
+      ventilacao.write(135);    // 75% abertura
+    } else {                         
+      ventilacao.write(180);    // 100% abertura
+    }
+
+    delay(1000); 
+}
